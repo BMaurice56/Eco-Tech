226011de12533804729204022c6b92a1b5166075
donoc <donocas@outlook.fr>
2024-05-06 22:30:40 +0200

    Ajout de la logique de changement de fragment

--- app/src/main/java/com/android/ecotech/MainActivity.java
+++ app/src/main/java/com/android/ecotech/MainActivity.java
@@ -1,12 +1,76 @@
 package com.android.ecotech;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.FrameLayout;
+import android.widget.ImageButton;
+import android.widget.LinearLayout;
+
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 public class MainActivity extends AppCompatActivity {
+    private FrameLayout frameLayout;
+    private LinearLayout linearLayout;
+
+    private ImageButton buttonHome;
+    private ImageButton buttonShopping;
+    private ImageButton buttonLoginRegister;
+    private ImageButton buttonParameter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        frameLayout = (FrameLayout) findViewById(R.id.frameLayout);
+        linearLayout = (LinearLayout) findViewById(R.id.layoutFooter);
+
+        buttonHome = findViewById(R.id.buttonHome);
+        buttonShopping = findViewById(R.id.buttonShopping);
+        buttonLoginRegister = findViewById(R.id.buttonLoginRegister);
+        buttonParameter = findViewById(R.id.buttonParameter);
+
+        getSupportFragmentManager().beginTransaction()
+                .replace(R.id.frameLayout, new FirstFragment())
+                .addToBackStack(null)
+                .commit();
+
+        buttonHome.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                replaceFragment(new FirstFragment());
+            }
+        });
+
+        buttonShopping.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                replaceFragment(new SecondFragment());
+            }
+        });
+
+        buttonLoginRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                replaceFragment(new ThirdFragment());
+            }
+        });
+
+        buttonParameter.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                replaceFragment(new FourthFragment());
+            }
+        });
+    }
+
+    private void replaceFragment(Fragment fragment) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frameLayout, fragment);
+        fragmentTransaction.commit();
     }
 }
