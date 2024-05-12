dde7925a264ec4ddb284658b26ea6e9e41691110
donoc <donocas@outlook.fr>
2024-05-12 14:54:55 +0200

    Ajout SnackBar et suppression commentaires inutiles

--- app/src/main/java/com/android/ecotech/ParameterFragment.java
+++ app/src/main/java/com/android/ecotech/ParameterFragment.java
@@ -1,44 +1,23 @@
 package com.android.ecotech;
 
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
+import android.widget.TextView;
+import androidx.fragment.app.Fragment;
+import com.google.android.material.snackbar.Snackbar;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link ParameterFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class ParameterFragment extends Fragment {
-
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
 
-    public ParameterFragment() {
-        // Required empty public constructor
-    }
+    public ParameterFragment() {}
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment ParameterFragment.
-     */
-    // TODO: Rename and change types and number of parameters
     public static ParameterFragment newInstance(String param1, String param2) {
         ParameterFragment fragment = new ParameterFragment();
         Bundle args = new Bundle();
@@ -51,7 +30,6 @@ public class ParameterFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         if (getArguments() != null) {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
@@ -60,16 +38,41 @@ public class ParameterFragment extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_parameter, container, false);
-        Spinner spinner = view.findViewById(R.id.spinner);
 
-        // Charger les langues depuis les ressources
+
+        Spinner spinner = view.findViewById(R.id.spinner);
         ArrayAdapter<CharSequence> adapter =
                 ArrayAdapter.createFromResource(requireContext(), R.array.languages, android.R.layout.simple_list_item_1);
 
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item); // Choisir le layout pour les options déroulantes
-        spinner.setAdapter(adapter); // Associer l'adaptateur au Spinner
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+
+
+
+        TextView textViewPol_Of_Conf = view.findViewById(R.id.textViewPol_Of_Conf);
+        TextView textViewParamData = view.findViewById(R.id.textViewParamData);
+        TextView textViewDelivery_Returns = view.findViewById(R.id.textViewDelivery_Returns);
+        TextView textViewFAQ = view.findViewById(R.id.textViewFAQ);
+        TextView textViewGeneral_Conditions = view.findViewById(R.id.textViewGeneral_Conditions);
+        TextView textViewLegalNotice = view.findViewById(R.id.textViewLegalNotice);
+
+        View.OnClickListener clickListener = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Snackbar.make(view, "Vous avez bien cliqué sur le lien", Snackbar.LENGTH_LONG).show();
+            }
+        };
+
+        textViewPol_Of_Conf.setOnClickListener(clickListener);
+        textViewParamData.setOnClickListener(clickListener);
+        textViewDelivery_Returns.setOnClickListener(clickListener);
+        textViewFAQ.setOnClickListener(clickListener);
+        textViewGeneral_Conditions.setOnClickListener(clickListener);
+        textViewLegalNotice.setOnClickListener(clickListener);
+
+
+
         return view;
     }
 }
