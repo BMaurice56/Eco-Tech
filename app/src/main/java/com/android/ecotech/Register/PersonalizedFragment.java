c158d298320ddd5b043e13ef38ca7149358bcd1f
donoc <donocas@outlook.fr>
2024-05-22 23:50:33 +0200

    Push

--- app/src/main/java/com/android/ecotech/Register/PersonalizedFragment.java
+++ app/src/main/java/com/android/ecotech/Register/PersonalizedFragment.java
@@ -1,7 +1,6 @@
 // Fragment personnalisé pour afficher un texte et d'autres éléments
 package com.android.ecotech.Register;
 
-import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
